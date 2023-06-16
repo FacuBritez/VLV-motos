c6e7dc712dfc41e6d422b6e7fc1fcbc6f7354751
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-16 18:47:40 -0300

    cambiando frameBorder por estar obsoleto

--- src/components/Nosotros.tsx
+++ src/components/Nosotros.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Nosotros() {
 
-    const year:number = new Date().getFullYear() - 2008; 
+    const year: number = new Date().getFullYear() - 2008;
 
     return (
         <>
@@ -22,13 +22,14 @@ function Nosotros() {
                                 Ofrecemos diagnóstico sin cargo para garantizar
                                 que su moto reciba la atención adecuada.
                                 <br />Si está buscando un taller mecánico de motos confiable
-                                    y profesional ¡Contáctenos hoy mismo!
+                                y profesional ¡Contáctenos hoy mismo!
                             </p>
                         </div>
                         <div className="col-lg-6 col-12 iframe">
                             <div>
                                 <iframe className="video" width="100%" height="315" src="https://www.youtube.com/embed/shj5StU1uWY"
-                                    title="YouTube video player" frameBorder="0"
+                                    title="YouTube video player"
+                                    style={{ border: "none" }}
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen></iframe>
                             </div>
